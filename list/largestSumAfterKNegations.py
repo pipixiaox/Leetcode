4076784404864a69523b54352da576c1fa4d1952
pipixiao <chenxiao@hust.edu.cn>
2022-08-05 10:26:43 +0800

    v2.0

--- list/largestSumAfterKNegations.py
+++ list/largestSumAfterKNegations.py
@@ -8,41 +8,15 @@ Type: +
 
 class Solution:
     def largestSumAfterKNegations(self, nums: list[int], k: int) -> int:
-        nums.sort()
-        if nums[0] >=0:
-            if k % 2 ==0:
-                return sum(nums)
-            else:
-                return sum(nums[1:])-nums[0]
-        
-        m = self.findMinusNum(nums)
-        if m < k:
-            if (k-m) % 2 == 0:
-                return sum(nums[m:])-sum(nums[0:m])
-            else:
-                # if m == len(nums):
-                #     return sum(nums[m-1:])-sum(nums[0:m-1])
-                if m == len(nums) or abs(nums[m-1]) < abs(nums[m]):
-                    return sum(nums[m-1:])-sum(nums[0:m-1])
-                else:
-                    return sum(nums[m+1:])-sum(nums[0:m+1])
-        else:
-            return sum(nums[k:])-sum(nums[0:k])
-        
-    def findMinusNum(self, nums):
-        left = 0
-        right = len(nums)
-        while left < right:
-            mid = (left+right) //2
-            if nums[mid] >=0 and nums[mid-1] <0:
-                return mid
-            elif nums[mid] <0:
-                left = mid +1
-                continue
-            else:
-                right = mid
-                continue
-        return left
+        nums.sort(key=abs, reverse = True)
+        for i in range(len(nums)):
+            if k >0 and nums[i] <0:
+                nums[i] *= -1
+                k -= 1
+        if k >0:
+            nums[-1] *= (-1) **k
+        return sum(nums)
+
 
 if __name__ == "__main__":
     so = Solution()
